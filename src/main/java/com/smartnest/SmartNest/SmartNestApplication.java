10778b01605cd3b8fcb2b34f1e608dbade3b6dcd
FaridIzquierdo <89101074+FaridIzquierdo@users.noreply.github.com>
2024-05-28 12:57:12 -0500

    Create the base of backend project

---
+++ src/main/java/com/smartnest/SmartNest/SmartNestApplication.java
@@ -0,0 +1,13 @@
+package com.smartnest.SmartNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartNestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartnest/SmartNest/SmartNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartnest.SmartNest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
